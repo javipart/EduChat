02a4bffd0396713382110d88749b3c666a4ff474
Javier Pardo <javiparto@gmail.com>
2020-02-23 16:40:02 -0500

    Modify cond

--- routes/inscription-route.js
+++ routes/inscription-route.js
@@ -92,7 +92,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const idStudent = mongoose.Types.ObjectId(req.params.id);
     let data;
-    await Inscription.find({ "idGroup": idStudent }).populate({
+    await Inscription.find({ "idStudent": idStudent }).populate({
         path: 'idGroup',
         populate: {
             path: 'idSubject'
