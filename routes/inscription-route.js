e2f79d7f6e55acc546928c66daeb8dcbaecb3bbb
Javier Pardo <javiparto@gmail.com>
2020-02-23 16:52:15 -0500

    remove populate

--- routes/inscription-route.js
+++ routes/inscription-route.js
@@ -92,7 +92,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const idStudent = mongoose.Types.ObjectId(req.params.id);
     let data;
-    await Inscription.find({ "idStudent": idStudent }).populate('idGroup').exec()
+    await Inscription.find({ "idStudent": idStudent })
         .then((inscriptions) => {
             data = {
                 status: 'ok',
